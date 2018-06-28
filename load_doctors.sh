ced685e655419bd376fb07d89651028cb47e920b
Joe Yerardi <joe.yerardi@gmail.com>
2018-06-28 10:30:31 -0400

    Fixed UPDATE query

--- load_doctors.sh
+++ load_doctors.sh
@@ -14,7 +14,7 @@ CREATE INDEX ON committee_members(state_name);
 CREATE INDEX ON committee_members(degree_1);
 CREATE INDEX ON committee_members(degree_2);"
 psql -U postgres -h db.fivetwentyseven.com -c "COPY nppes FROM STDIN WITH CSV HEADER DELIMITER ',' QUOTE '\"'" < "data/npidata_pfile_20050523-20180513.csv" health
-psql -U postgres -h db.fivetwentyseven.com -d health -c "UPDATE committee_members
+psql -U postgres -h db.fivetwentyseven.com -d health -c "UPDATE nppes
 SET provider_first_name = TRIM(UPPER(provider_first_name)),
     provider_last_name_legal_name = TRIM(UPPER(provider_last_name_legal_name)),
     provider_business_practice_location_address_state_name = TRIM(UPPER(provider_business_practice_location_address_state_name));"
